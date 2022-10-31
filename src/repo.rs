40296325e74b006f1bcfc05eee39e5afc536066f
Jake Ireland <jakewilliami@icloud.com>
2022-10-31 17:56:39 +1300

    Clean up gl repository
    There is still plenty of code that can be optimised, but I removed
    old, uneccesary comments, as well as making some small optimisations,
    or changing things to be more idiomatic.
    
    Addresses #2

--- src/repo.rs
+++ src/repo.rs
@@ -1,6 +1,5 @@
 use std::process::{Command, Stdio};
 use std::path::{Path, PathBuf};
-use std::ffi::OsString;
 
 pub fn current_repository() -> Option<String> {
 	let mut cmd = Command::new("git");
@@ -13,6 +12,7 @@ pub fn current_repository() -> Option<String> {
 	
 	if output.status.success() {
 		let mut current_repo_path = String::from_utf8_lossy(&output.stdout).into_owned();
+		
 		// strip the output of any new lines
 		if current_repo_path.ends_with('\n') {
 			current_repo_path.pop();
@@ -20,17 +20,20 @@ pub fn current_repository() -> Option<String> {
             	current_repo_path.pop();
         	}
 		}
+		
 		// get the basename of the path
 		let repo_path = Path::new(&current_repo_path);
 		let repo_path_basename = PathBuf::from(
 			repo_path.file_name().unwrap()
 		);
+		
 		let repo_basename_str: String = repo_path_basename
 			.into_os_string()
 			.into_string()
 			.unwrap();
-		return Some(repo_basename_str);
+		
+		Some(repo_basename_str)
 	} else {
-		return None
+		None
 	}
 }
