700f6c292404784cc4df42c04683d32de9b000a7
Jake Ireland <jakewilliami@icloud.com>
2022-11-07 18:50:28 +1300

    Update dependencies (including clap)
    No functionality changes, but some back-end changes, especially to
    clap, which I updated from v2 to v4.  v4 introduced a
    whole bunch of breaking changes, but I have been working
    
    While I have fixed all compiler errors to do with the breaking
    changes, I may need to go through and do some testing to ensure that
    all of the app's functionality is not panicing.
    
    I really like clap's new API.  It seems really logical.  In v2, you
    can tell that a lot of the functionality that was present was made on
    top of an already existing API, and nobody wanted to change anything
    fundamentally with clap.  The new verson does away with methods such
    as `min_args`, `max_args`, `multiple`, and `takes_value`, with
    essentially one replacement: `num_args`, which takes a number or
    range.  For example, if I make an argument with `num_args(0)`, this
    _implies_ `multiple(false)`, `min_args(0)`, and `max_args(0)`.  If I
    make an argument with `num_args(1..=2)`, this implies
    `multiple(true)`, `min_args(1)`, and `max_args(2)`.  If I make an
    argument with `num_args(0..=1)`, this implies `multiple(false)`,
    `min_args(0)`, and `max_args(1)`.  The new version seems to condense a
    whole lot of other logic, too, into a really clean API.

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ mod repo;
 mod status;
 
 extern crate clap;
-use clap::{Arg, App, value_t, crate_version};
+use clap::{Arg, Command, crate_version};
 
 // needed for log.rs
 extern crate colored;
@@ -24,11 +24,11 @@ extern crate chrono;
 // Also, I have notes on github-linguist which I could add to this app, maybe under a `help` subcommand?
 
 fn main() {
-	let matches = App::new("gl")
+	let matches = Command::new("gl")
                             .version(crate_version!())
                             .author("Jake W. Ireland. <jakewilliami@icloud.com>")
                             .about("Git log and other personalised git utilities.  By default (i.e., without any arguments), it will print the last 10 commits nicely.")
-							.arg(Arg::with_name("LOGNUMBER")
+							.arg(Arg::new("LOGNUMBER")
 								// TODO: as well as -n we should also be able to do -10, -100, -3, etc
 								// .short("n")
 								// .long("number")
@@ -36,78 +36,67 @@ fn main() {
 								.help("Given a number, will print the last n commits nicely.")
 								// .index(1)
 								// .default_value("10")
-								.takes_value(true)
+								.num_args(1)
 								.required(false)
-								.multiple(false)
 						   	)
-							.arg(Arg::with_name("LANGUAGES")
-								.short("l")
+							.arg(Arg::new("LANGUAGES")
+								.short('l')
 								.long("languages")
 								.help("Prints language breakdown in present repository.  Will print only top n languages if given value (optional).")
-								.takes_value(true)
-						        .min_values(0)
+								.num_args(0..=1)
 								.required(false)
-								.multiple(false)
 						   	)
-							.arg(Arg::with_name("STATUS")
-								.short("s")
+							.arg(Arg::new("STATUS")
+								.short('s')
 								.long("status")
 								.help("Prints current git status minimally.")
-								.takes_value(true)
-								.min_values(0)
+								.num_args(0..=1)
 								.required(false)
-								.multiple(false)
 						   	)
-							.arg(Arg::with_name("GLOBAL")
-								.short("g")
+							.arg(Arg::new("GLOBAL")
+								.short('g')
 								.long("global")
 								.help("Gets git status for any dirty repositories, defined from file")
-								.takes_value(false)
+								.num_args(0)
 								.required(false)
-								.multiple(false)
 						   	)
-							.arg(Arg::with_name("BRANCH")
-								.short("b")
+							.arg(Arg::new("BRANCH")
+								.short('b')
 								.long("branch")
 								.help("Prints the current branch name")
-								.takes_value(false)
+								.num_args(0)
 								.required(false)
-								.multiple(false)
 						   	)
-							.arg(Arg::with_name("BRANCHES")
-								.short("B")
+							.arg(Arg::new("BRANCHES")
+								.short('B')
 								.long("branches")
 								.help("Prints all local branches in the current repository")
-								.takes_value(false)
+								.num_args(0)
 								.required(false)
-								.multiple(false)
 						   	)
-							.arg(Arg::with_name("REMOTES")
-								.short("R")
+							.arg(Arg::new("REMOTES")
+								.short('R')
 								.long("remotes")
 								.help("Prints remote branches of the current repository")
-								.takes_value(false)
+								.num_args(0)
 								.required(false)
-								.multiple(false)
 						   	)
-							.arg(Arg::with_name("REPO")
-								.short("r")
+							.arg(Arg::new("REPO")
+								.short('r')
 								.long("repo")
 								.help("Prints the name of the current repository")
-								.takes_value(false)
+								.num_args(0)
 								.required(false)
-								.multiple(false)
 						   	)
-							.arg(Arg::with_name("COMMITCOUNT")
-								.short("c")
+							.arg(Arg::new("COMMITCOUNT")
+								.short('c')
 								.long("commit-count")
 								.help("Counts the current number of commits on working branch on the current day")
 								.required(false)
-								.multiple(false)
 								.conflicts_with("COMMITCOUNTWHEN")
 						   	)
-							.arg(Arg::with_name("COMMITCOUNTWHEN")
-								.short("C")
+							.arg(Arg::new("COMMITCOUNTWHEN")
+								.short('C')
 								.long("commit-count-when")
 								.help("Counts the number of commits for a specified day.  Takes values \"today\" (see -c), \"yesterday\", or some number of days ago.")
 								// TODO:
@@ -116,58 +105,58 @@ fn main() {
 								//   number of commits in the last 5 days ago, no including anything since yesterday.  This can be done by calculating commits_since(5)
 								//   - commits_since(2), etc.  To do this I need to figure out how to use multiple arguments, otherwise I will have to create a separate
                                 //   flag
-								.takes_value(true)
+								.num_args(1)
 								.required(false)
-								// .multiple(true)
+								// .multiple_values(true)
 								.conflicts_with("COMMITCOUNT")
 						   	)
-							.arg(Arg::with_name("AUTHORCOMMITCOUNTS")
-								.short("A")
+							.arg(Arg::new("AUTHORCOMMITCOUNTS")
+								.short('A')
 								.long("author-commit-counts")
 								.help("Prints the number of commits per author")
-								.takes_value(false)
+								.num_args(0)
 								.required(false)
-								.multiple(false)
 						   	)
-							.arg(Arg::with_name("AUTHORCONTRIBSTATS")
-								.short("S")
+							.arg(Arg::new("AUTHORCONTRIBSTATS")
+								.short('S')
 								.long("author-contrib-stats")
 								.help("Prints some contribution statistics given an author")
-								.takes_value(false)
+								.num_args(0)
 								.required(false)
-								.multiple(false)
 						   	)
 							.get_matches();
 
 
 	// show the git log
-	if matches.args.is_empty() {
+	if !matches.args_present() {
 		let n: usize = 10;
 		log::get_git_log(n);
 		return;
 	}
-	if matches.is_present("LOGNUMBER") {
-		let n = value_t!(matches, "LOGNUMBER", usize)
-			.unwrap_or(0);
-		if n != 0 {
-			log::get_git_log(n);
+	if matches.get_flag("LOGNUMBER") {
+		let n = matches.get_one::<usize>("LOGNUMBER")
+			.unwrap_or(&0);
+		if n != &0 {
+			log::get_git_log(*n);
 		}
 	}
 
 	// show languages
-	if matches.is_present("LANGUAGES") {
+	if matches.get_flag("LANGUAGES") {
 		// This parses _and_ prints the language output
 		// languages::parse_language_data();
 		let language_summary = languages::construct_language_summary();
-		let top_n = value_t!(matches, "LANGUAGES", usize)
+		let top_n = matches.get_one::<usize>("LANGUAGES")
+			.map(|n| n.to_owned())
 			.unwrap_or(language_summary.len());
 		languages::print_language_summary(top_n, language_summary);
 	};
 
 	// show status of git repo
-	if matches.is_present("STATUS") {
-		let dir = value_t!(matches, "STATUS", String)
-			.unwrap_or_else(|_| "".to_string());
+	if matches.get_flag("STATUS") {
+		let dir = matches.get_one::<String>("STATUS")
+			.map(|n| n.to_string())
+			.unwrap_or_else(|| "".to_string());
 		let maybe_dir = if dir.is_empty() {
 			None
 		} else {
@@ -177,12 +166,12 @@ fn main() {
 	};
 
 	// show statuses of predefined git repos
-	if matches.is_present("GLOBAL") {
+	if matches.get_flag("GLOBAL") {
 		status::global_status();
 	};
 
 	// show branch name
-	if matches.is_present("BRANCH") {
+	if matches.get_flag("BRANCH") {
 		let current_branch = branch::current_branch();
 		if current_branch.is_some() {
 			println!("{}", current_branch.unwrap());
@@ -190,12 +179,12 @@ fn main() {
 	}
 
 	// show branches
-	if matches.is_present("BRANCHES") {
+	if matches.get_flag("BRANCHES") {
 		branch::get_branch_names(branch::BranchListings::Local);
 	}
 
 	// show the current repository
-	if matches.is_present("REPO") {
+	if matches.get_flag("REPO") {
 		let current_repo = repo::current_repository();
 		if current_repo.is_some() {
 			println!("{}", current_repo.unwrap());
@@ -203,28 +192,30 @@ fn main() {
 	}
 
 	// show remote branches
-	if matches.is_present("REMOTES") {
+	if matches.get_flag("REMOTES") {
 		branch::get_branch_names(branch::BranchListings::Remotes);
 	}
 
 	// show commit count
-	if matches.is_present("COMMITCOUNT") {
+	if matches.get_flag("COMMITCOUNT") {
 		commitcount::get_commit_count("today");
 	}
 
-	if matches.is_present("COMMITCOUNTWHEN") {
-		let input_raw = matches.value_of("COMMITCOUNTWHEN");
+	if matches.get_flag("COMMITCOUNTWHEN") {
+		let input_raw = matches.get_one::<&str>("COMMITCOUNTWHEN");
 
 		let input = input_raw.unwrap();
 		commitcount::get_commit_count(input);
 	}
 
-	if matches.is_present("AUTHORCOMMITCOUNTS") {
+	// show number of commits per author, sorted by commit
+	if matches.get_flag("AUTHORCOMMITCOUNTS") {
 		let contributors = contributions::git_contributor_stats();
 		contributions::display_git_author_frequency(contributors);
 	}
 
-	if matches.is_present("AUTHORCONTRIBSTATS") {
+	// show contribution stats per author, sorted by lines added + deleted
+	if matches.get_flag("AUTHORCONTRIBSTATS") {
 		let contributors = contributions::git_contributor_stats();
 		contributions::display_git_contributions_per_author(contributors);
 	}
