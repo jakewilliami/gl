1918304fdeb4045c85d5a4aabdca6e7771406e35
Jake Ireland <jakewilliami@icloud.com>
2024-10-29 01:12:28 +1300

    Add additional personalised alias to static identities!  v3.0.1
    I found the commit name `jakeireland` in one of my old repositories.
    I needed to add this to the constant list of (configurable) identities
    so that `gl` will format logs how I like, when the commiter is me.

--- src/config.rs
+++ src/config.rs
@@ -1,10 +1,11 @@
 // Git log
 // Update this list for your own identity!
-pub const ME_IDENTITY: [&str; 4] = [
+pub const ME_IDENTITY: [&str; 5] = [
     "jakewilliami",
     "Jake Ireland",
     "Jake W. Ireland",
     "jakewilliami@icloud.com",
+    "jakeireland",
 ];
 
 // Top n results
